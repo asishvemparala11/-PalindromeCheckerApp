cd1e3af251368e933aaca5e26c756a12ecbd6f07
asishvemparala11 <asishvemparala@gmail.com>
2026-02-20 10:10:57 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,20 @@
+
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome Checker ");
+        System.out.println("=================================");
+
+        // Application details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("Developer        : Console-based Java App");
+
+        // End of UC1
+        System.out.println("\nApplication started successfully.");
+    }
+
+
+
