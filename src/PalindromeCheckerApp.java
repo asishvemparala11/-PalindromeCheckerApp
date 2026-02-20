8a88e185ad84bbaa2179dcd4d9b3bbe9845aff4f
asishvemparala11 <asishvemparala@gmail.com>
2026-02-20 10:38:58 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+         {
 
+                // Create Scanner object FIRST
+                Scanner sc = new Scanner(System.in);
 
+                // Take input from user
+                System.out.print("Enter a string: ");
+                String input = sc.nextLine();
+
+                // Display input (for verification)
+                System.out.println("You entered: " + input);
+
+            }
+        }
     }
-}
+
+
+
+
 
 
